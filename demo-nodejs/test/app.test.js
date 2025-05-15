f2a6e2832f1d22ca169c7cb7468e1973239a2b11
zsche <zscheer00@gmail.com>
2025-05-15 09:38:40 -0700

    Initial commit of Node.js project

---
+++ demo-nodejs/test/app.test.js
@@ -0,0 +1,15 @@
+// demo-nodejs/app.js
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World from Demo Node.js App!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
